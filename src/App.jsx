dbe1f6cca770a8183f5219e938ee8b228360896f
TanuDubey-13 <2k24.cs1q.2413911@gmail.com>
2026-07-11 14:27:02 +0530

    Fix: Switch to HashRouter for sub-path routing on GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { ThemeProvider } from './context/ThemeContext';
 import { ProtectedRoute } from './routes/ProtectedRoute';
@@ -46,7 +46,7 @@ export default function App() {
   return (
     <ThemeProvider>
       <AuthProvider>
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             {/* 1. Public Authentication Routes */}
             <Route 
@@ -185,7 +185,7 @@ export default function App() {
             draggable 
             theme="colored"
           />
-        </BrowserRouter>
+        </HashRouter>
       </AuthProvider>
     </ThemeProvider>
   );
